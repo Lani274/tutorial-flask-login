ed5024f853be4b536cf12108f85205d867363ec4
Lani274 <aramlana274@yahoo.de>
2023-04-24 22:33:30 +0200

    change tutorial

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -10,8 +10,27 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-     return render_template("sign_up.html")
+     if request.method =='POST':
+         email = request.form.get('email')
+         firstName = request.form.get('firstName')
+         password1 = request.form.get('password1')
+         password2 = request.form.get('password2')
+         
+         if len(email) < 4:
+          flash('Email must be greater than 3 characters.', category='error')
+         elif len(firstName) < 2:
+          flash('First name must be greater than 1 character.', category='error')
+         elif password1 != password2:
+          flash('Passwords dont match.', category='error')
+         elif len(password1) < 7:
+          flash('password must be at least 7 characters.', category='error')
+         else:
+          flash('Account created!.', category='success')
+         
+          return render_template("sign_up.html")
+
+
 # log in, log out and sign in Navigierung mit / nach der URL Tetx wird ausgegeben
 # how to set up the routes and URls in flask
