df33e9eec551b828d9b2230706e42231d4b24b79
Lani274 <aramlana274@yahoo.de>
2023-04-25 01:31:26 +0200

    last change code

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME ="database.db"
@@ -11,6 +12,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+
     from .views import views
     from .auth import auth
 
@@ -19,13 +21,21 @@ def create_app():
 
     from .models import User, Note
 
-    create_database(app)
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+
 
 
 
